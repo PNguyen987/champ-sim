9d398c21b53a34e79f365005841167a0e7b8e2f3
PNguyen987 <pnguyen@nevada.unr.edu>
2022-05-14 11:33:21 -0700

    starting react stuff

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+return (
+	<div>
+	<h1>
+		Site built by Phi Nguyen to learn react. Starting off by building a champ select simulator
+	</h1>
+	</div>
+);
+};
+
+export default About;
---
+++ src/pages/simulator.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Simulator = () => {
+return (
+	<h1>Simulator</h1>
+);
+};
+
+export default Simulator;
